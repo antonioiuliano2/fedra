95cb5db0fb5c56935207bd940fb32943f406f9e6
valeri <valeri@96872706-0045-0410-9534-84be8447c86c>
2003-07-03 17:54:17 +0000

    *** empty log message ***
    git-svn-id: http://emulsion.na.infn.it/svn/fedra/trunk@36 96872706-0045-0410-9534-84be8447c86c

--- src/appl/align/align.C
+++ src/appl/align/align.C
@@ -1,33 +1,6 @@
 EdbPVRec    *ali=0;
 
 //------------------------------------------------------------
-int cut_seg_bern(float x0, float y0, float tx0, float ty0, float puls )
-{
-  if( TMath::Abs(tx0) < .1 ) return 0;
-  if( TMath::Abs(ty0) > .1 ) return 0;
-  if( puls < 15 )    return 0;
-
-  return 1;
-}
-
-//------------------------------------------------------------
-int cut_seg_nap(float x0, float y0, float tx0, float ty0, float puls )
-{
-  if( x0 > -60000. )  return 0;
-  if( y0 >  50000. )  return 0;
-
-  return 1;
-}
-
-//------------------------------------------------------------
-int cut_seg_cp(float x0, float y0, float tx0, float ty0, float puls )
-{
-  // this function is called by GetDataCouples() to reject bad segments
-
-  return    cut_seg_nap(x0,y0,tx0,ty0,puls);
-}
-
-//------------------------------------------------------------
 void align()
 {
   gROOT->LoadMacro("ScanCond.C");
@@ -64,9 +37,32 @@ void align()
 
 
   filltree(tree, ali,1);
-  //delete pvol;
-  //delete ali;
-  
+}
+
+//------------------------------------------------------------
+int cut_seg_bern(float x0, float y0, float tx0, float ty0, float puls )
+{
+  if( TMath::Abs(tx0) < .1 ) return 0;
+  if( TMath::Abs(ty0) > .1 ) return 0;
+  if( puls < 15 )    return 0;
+
+  return 1;
+}
+
+//------------------------------------------------------------
+int cut_seg_nap(float x0, float y0, float tx0, float ty0, float puls )
+{
+  if( x0 > -60000. )  return 0;
+  if( y0 >  50000. )  return 0;
+
+  return 1;
+}
+
+//------------------------------------------------------------
+int cut_seg_cp(float x0, float y0, float tx0, float ty0, float puls )
+{
+  // this function is called by GetDataCouples() to reject bad segments
+  return    cut_seg_nap(x0,y0,tx0,ty0,puls);
 }
 
 //-------------------------------------------------------------------------------
@@ -94,7 +90,6 @@ void getBernData(EdbPVRec    *al)
   EdbPattern *pat=0;
   pat = new EdbPattern(0.,0., 0. );
   getDataCouples(
-  		 //		 "/mnt/operalabdb_e/data/proc/b1_jun2003/pl14/lnk_Jun2003_02010114.root"
   		 "/mnt/nusrv4_2/ftpuser/aedb1204.root"
   		 ,"couples", pat );
   al->AddPattern(pat);
@@ -102,7 +97,6 @@ void getBernData(EdbPVRec    *al)
 
   pat = new EdbPattern(0.,0., 1100. );
   getDataCouples(
-		 //		 "/mnt/operalabdb_e/data/proc/b1_jun2003/pl15/lnk_Jun2003_02010115.root"
 		 "/mnt/nusrv4_2/ftpuser/aedb1304.root"
 		 ,"couples", pat );
   al->AddPattern(pat);
