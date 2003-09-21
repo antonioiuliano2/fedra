e8b2f569267c048d100e3944d3c663e5f187b7b6
valeri <valeri@96872706-0045-0410-9534-84be8447c86c>
2003-09-21 06:46:47 +0000

    link | align | track
    git-svn-id: http://emulsion.na.infn.it/svn/fedra/trunk@74 96872706-0045-0410-9534-84be8447c86c

--- src/appl/recset/recset.cpp
+++ src/appl/recset/recset.cpp
@@ -3,14 +3,22 @@
 
 int main(int argc, char* argv[])
 {
-  if (argc < 2)
+  if (argc < 3)
     {
-      cout<< "usage: \n \trecset input_data_set_file \n\n";
+      cout<< "usage: \n \trecset { link | align | track} input_data_set_file \n\n";
       return 0;
     };
-  char* name = argv[1];
+  char *key  = argv[1];
+  char *name = argv[2];
 
   EdbDataProc proc(name);
-  proc.Process();
+
+  if(!strcmp(key,"link")  ) {
+    proc.Link();
+  } else if(!strcmp(key,"align")) {
+    proc.Align();
+  } else if(!strcmp(key,"track")) {
+    proc.LinkTracks();
+  }
   return 0;
 }
