ae23f952c998d85400ce77a9375f69d4fec7e775
valeri <valeri@96872706-0045-0410-9534-84be8447c86c>
2003-11-18 11:33:16 +0000

    check root version when build
    git-svn-id: http://emulsion.na.infn.it/svn/fedra/trunk@129 96872706-0045-0410-9534-84be8447c86c

--- win32/makeall.cmd
+++ win32/makeall.cmd
@@ -5,50 +5,39 @@
 setlocal
 
 if /I '%1'=='clean' goto CLEAN
+if /I '%1'=='chktgt' goto CHECKTARGET
+if /I '%1'=='archive' goto ARCHIVE
 
 ::----------------------------------------------------------------------
-:BUILD
+:START
 	SET CURRENTDIR=%CD%
 
-:: Check if ROOT is well installed
+:: Archive old fedra binaries
+	if exist lib\*.* call %0 ARCHIVE
+	
+ECHO Check if ROOT is installed 
 	if not defined ROOTSYS goto ROOTSYS_NOT_DEFINED
 	if not exist %ROOTSYS%\bin\root.exe goto ROOT_DONT_EXIST
+	ECHO ok!
 
-:: Archive old FEDRA binaries
-	if not exist lib\*.* goto NOTARCHIVE
-	
-   :LOOP
-	SET Choice=
-	SET /P Choice=Previous FEDRA binaries exist, do you want to archive them (Y/N)? 
-	IF NOT '%Choice%'=='' SET Choice=%Choice:~0,1%
-	ECHO.
-	IF /I '%Choice%'=='Y' GOTO SAVEARCHIVE
-	IF /I '%Choice%'=='N' GOTO NOTARCHIVE
-   GOTO LOOP
-	
-   :SAVEARCHIVE
-   ECHO.
-   ECHO The files will be saved in %cd%\old\"folder"
-	SET Choice=
-	SET /P Choice=Insert the folder name:
-	ECHO.
-	mkdir old
-	mkdir old\%Choice%
-	mkdir old\%Choice%\bin
-	mkdir old\%Choice%\lib
-	mkdir old\%Choice%\tools
-	move  bin\*.*   old\%Choice%\bin
-	move  lib\*.*   old\%Choice%\lib
-	copy  tools\*.* old\%Choice%\tools 
-	ECHO Files saved. If you want to use these files, change the environment variables
-	ECHO using %CD%\old\%Choice%\tools\setfedra -u
-	echo.
-	pause
-	echo.
+ECHO Check the version of ROOT
+       IF NOT EXIST workspace\rootcompare.txt GOTO ROOTISCHANGED
+       dir %ROOTSYS%\bin |find "." > rootcompare.temp
+       fc workspace\rootcompare.txt rootcompare.temp > temp.temp
+       if %ERRORLEVEL%==0 GOTO ROOTISNOTCHANGED
 
-:NOTARCHIVE
-	::if exist lib\*.* del /q lib\*.*	
-	::if exist bin\*.* del /q bin\*.*
+	:ROOTISCHANGED
+		ECHO The version of ROOT has been changed, I will remove FEDRA binaries...
+		call %0 clean
+		goto ROOTCHECKEND
+	:ROOTISNOTCHANGED
+		ECHO ok!
+	:ROOTCHECKEND
+		if exist temp.temp del temp.temp
+		if exist rootcompare.temp del rootcompare.temp
+::-----------------------------------------------------------------------------
+:BUILDFEDRA
+	dir %ROOTSYS%\bin |find "." > workspace\rootcompare.txt
 
 ECHO Remove old FEDRA environment variables
 	cd tools
@@ -74,42 +63,24 @@ ECHO Set the environment variables
 	cd tools
 	call setfedra -u
 	cd %CURRENTDIR%
+	set FEDRA=%CURRENTDIR%
+	set path=%CURRENTDIR%\bin;%CURRENTDIR%\lib;%PATH% 
 	ECHO Environment variables have been changed only for the current user.
 	ECHO Other users should set their own variables using setfedra utility.
 	ECHO.
 
 ECHO Check Target:
 ECHO -------------
-SET CHKFILE=lib\libEdb.lib
-        IF EXIST %CHKFILE% ECHO     %CHKFILE%...ok!
-        IF NOT EXIST %CHKFILE% ECHO     %CHKFILE%...ERROR
-SET CHKFILE=lib\libEdb.dll
-        IF EXIST %CHKFILE% ECHO     %CHKFILE%...ok!
-        IF NOT EXIST %CHKFILE% ECHO     %CHKFILE%...ERROR
-SET CHKFILE=lib\libEmath.lib
-        IF EXIST %CHKFILE% ECHO     %CHKFILE%...ok!
-        IF NOT EXIST %CHKFILE% ECHO     %CHKFILE%...ERROR
-SET CHKFILE=lib\libEmath.dll
-        IF EXIST %CHKFILE% ECHO     %CHKFILE%...ok!
-        IF NOT EXIST %CHKFILE% ECHO     %CHKFILE%...ERROR
-SET CHKFILE=lib\libEdr.lib
-        IF EXIST %CHKFILE% ECHO     %CHKFILE%...ok!
-        IF NOT EXIST %CHKFILE% ECHO     %CHKFILE%...ERROR
-SET CHKFILE=lib\libEdr.dll
-        IF EXIST %CHKFILE% ECHO     %CHKFILE%...ok!
-        IF NOT EXIST %CHKFILE% ECHO     %CHKFILE%...ERROR
-SET CHKFILE=lib\libEdd.lib
-        IF EXIST %CHKFILE% ECHO     %CHKFILE%...ok!
-        IF NOT EXIST %CHKFILE% ECHO     %CHKFILE%...ERROR
-SET CHKFILE=lib\libEdd.dll
-        IF EXIST %CHKFILE% ECHO     %CHKFILE%...ok!
-        IF NOT EXIST %CHKFILE% ECHO     %CHKFILE%...ERROR
-SET CHKFILE=bin\rwc2edb.exe
-        IF EXIST %CHKFILE% ECHO     %CHKFILE%...ok!
-        IF NOT EXIST %CHKFILE% ECHO     %CHKFILE%...ERROR
-SET CHKFILE=bin\recset.exe
-        IF EXIST %CHKFILE% ECHO     %CHKFILE%...ok!
-        IF NOT EXIST %CHKFILE% ECHO     %CHKFILE%...ERROR
+call %0 chktgt lib\libEdb.lib
+call %0 chktgt lib\libEdb.dll
+call %0 chktgt lib\libEmath.lib
+call %0 chktgt lib\libEmath.dll
+call %0 chktgt lib\libEdr.lib
+call %0 chktgt lib\libEdr.dll
+call %0 chktgt lib\libEdd.lib
+call %0 chktgt lib\libEdd.dll
+call %0 chktgt bin\rwc2edb.exe
+call %0 chktgt bin\recset.exe
 
 	ECHO.
 	ECHO NOTES:
@@ -120,6 +91,7 @@ SET CHKFILE=bin\recset.exe
 	pause
 	goto STOP
 
+::------ SUBROUTINES
 
 ::----------------------------------------------------------------------
 :ROOTSYS_NOT_DEFINED
@@ -140,8 +112,44 @@ SET CHKFILE=bin\recset.exe
 	IF EXIST bin RD /S/Q bin
 	IF EXIST lib RD /S/Q lib
 	IF EXIST workspace\obj RD /S/Q workspace\obj
+	IF EXIST workspace\rootcompare.txt DEL workspace\rootcompare.txt
 	ECHO ok!
+	goto STOP
+::----------------------------------------------------------------------
+:CHECKTARGET
+        IF EXIST     %2 ECHO 	%2...ok!
+        IF NOT EXIST %2 ECHO 	%2...ERROR
+	  goto STOP
+::----------------------------------------------------------------------
+:ARCHIVE
+   :LOOP
+	SET Choice	SET /P Choice=Previous FEDRA binaries exist, do you want to archive them (Y/N)? 
+	IF NOT '%Choice%'=='' SET Choice=%Choice:~0,1%
+	ECHO.
+	IF /I '%Choice%'=='Y' GOTO SAVEARCHIVE
+	IF /I '%Choice%'=='N' GOTO STOP
+   GOTO LOOP
+	
+   :SAVEARCHIVE
+   ECHO.
+   ECHO The files will be saved in %cd%\old\"folder"
+	SET Choice	SET /P Choice=Insert the folder name:
+	ECHO.
+	mkdir old
+	mkdir old\%Choice%
+	mkdir old\%Choice%\bin
+	mkdir old\%Choice%\lib
+	mkdir old\%Choice%\tools
+	move  bin\*.*   old\%Choice%\bin
+	move  lib\*.*   old\%Choice%\lib
+	copy  tools\*.* old\%Choice%\tools 
+	ECHO Files saved. If you want to use these files, change the environment variables
+	ECHO using %CD%\old\%Choice%\tools\setfedra -u
+	echo.
+	pause
+	echo.
 	goto STOP
 ::----------------------------------------------------------------------
 :STOP
 endlocal
+)
