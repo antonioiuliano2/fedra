e5400fb69acabeae72bb99ec15578f0cb205908f
valeri <valeri@96872706-0045-0410-9534-84be8447c86c>
2004-02-12 05:54:00 +0000

    *** empty log message ***
    git-svn-id: http://emulsion.na.infn.it/svn/fedra/trunk@194 96872706-0045-0410-9534-84be8447c86c

--- src/appl/vertex/read_tracks.C
+++ src/appl/vertex/read_tracks.C
@@ -1,12 +1,89 @@
 using namespace MATRIX;
 using namespace VERTEX;
 
+
+//----------------------------------------------------------------------------
+void read_tracks()
+{
+  EdbVertex v;
+  v.Test();
+}
+
+//----------------------------------------------------------------------------
+void test_tracks(const char *file_name="linked_tracks.root")
+{
+  EdbTrackP *track  = new EdbTrackP(8);
+  TTree *tracks = init_tracks(file_name, track);
+
+  //test_ms();
+
+  
+  int nentr= tracks->GetEntries();
+  //int nentr= 10;
+
+  int nseg=1;
+  for(int i=0; i<nentr; i++) {
+    tracks->GetEntry(i);
+
+    if(track->N()>nseg) {
+      printf("track %d  has %d segments\n", i, track->N());
+      track->COV().Print();
+      nseg++;
+    }
+  }
+  
+}
+
+
+
+//----------------------------------------------------------------------------
+void test_ms(TCut cut="nseg==10")
+{
+  TCanvas *c1=new TCanvas();
+  c1->Divide(2,2);
+  c1->cd(1);
+  tracks->Draw("(t.eY+t.eTY*(s.eZ-t.eZ))-s.eY:s.eZ-t.eZ",cut);
+  c1->cd(2);
+  tracks->Draw("(t.eX+t.eTX*(s.eZ-t.eZ))-s.eX:s.eZ-t.eZ",cut);
+  c1->cd(3);
+  tracks->Draw("(t.eY+t.eTY*(s.eZ-t.eZ))-s.eY:(t.eX+t.eTX*(s.eZ-t.eZ))-s.eX:s.eZ-t.eZ",cut);
+  c1->cd(4);
+  tracks->Draw("s.eTY:s.eTX:s.eZ","nseg>10&&abs(s.eTY-0.02)<0.04&&abs(s.eTX+0.06)<0.04");
+}
+
+//----------------------------------------------------------------------------
+void test_fit(const char *file_name="linked_tracks.root")
+{
+  EdbTrackP *track  = new EdbTrackP(8);
+  TTree     *tracks = init_tracks(file_name, track);
+
+  float sx=5., sy=5., sz=0., stx=0.006, sty=0.006, sp=0.1;
+  float p=1.;
+
+  //int nentr= tracks1->GetEntries();
+  int nentr= 10;
+  
+  for(int i=0; i<nentr; i++) {
+    tracks->GetEntry(i);
+
+    printf("track %d  has %d segments\n", i, track->N());
+
+    for(int j=0; j<track->N(); j++) 
+      (track->GetSegment(j))->SetErrors(sx*sx,sy*sy,sz*sz,stx*stx,sty*sty,sp*sp);
+
+    track->FitTrackKF();
+    track->Print();
+  }
+
+}
+
+//----------------------------------------------------------------------------
 TTree *init_tracks(const char *file_name, EdbTrackP *track)
 {
   char *tree_name="tracks";
   TFile *f = new TFile(file_name);
   if (f)  tracks = (TTree*)f->Get(tree_name);
-  if(!tracks) return;
+  if(!tracks) return 0;
   f->cd();
 
   int nseg,trid,npl,n0;
@@ -25,71 +102,3 @@ TTree *init_tracks(const char *file_name, EdbTrackP *track)
   return tracks;
 }
 
-void read_tracks(const char *file_name="linked_tracks.root")
-{
-  EdbTrackP *track1  = new EdbTrackP(8);
-  TTree     *tracks1 = init_tracks(file_name, track1);
-
-  EdbTrackP *track2  = new EdbTrackP(8);
-  TTree     *tracks2 = init_tracks(file_name, track2);
-
-  float sx=5., sy=5., sz=0., stx=0.006, sty=0.006, sp=0.1;
-  float p=1.;
-
-  float x1,y1,z1,tx1,ty1;
-  float x2,y2,z2,tx2,ty2;
-
-
-  //int nentr= tracks1->GetEntries();
-  int nentr= 10;
-  
-  for(int i1=0; i1<nentr; i1++) {
-    tracks1->GetEntry(i1);
-    track1->SetErrors(sx*sx,sy*sy,sz*sz,stx*stx,sty*sty,sp*sp);
-
-    x1 = track1->X();
-    y1 = track1->Y();
-    z1 = track1->Z();
-    tx1 = track1->TX();
-    ty1 = track1->TY();
-
-    VtVector p1( x1,  y1,  z1,  tx1,  ty1, 1.);
-    CMatrix  c1(track1->COV());
-    t1 = new Track(p1,c1);
-
-    for(int i2=0; i2<nentr; i2++) {
-      tracks2->GetEntry(i2);
-      track2->SetErrors(sx*sx,sy*sy,sz*sz,stx*stx,sty*sty,sp*sp);
-
-
-      x2 = track2->X();
-      y2 = track2->Y();
-      z2 = track2->Z();
-      tx2 = track2->TX();
-      ty2 = track2->TY();
-
-      VtVector p2( x2,  y2,  z2,  tx2,  ty2, 1.);
-      CMatrix  c2(track2->COV());
-      t2 = new Track(p2,c2);
-
-      v = new Vertex(*t1, *t2);
-      v->findVertexVt();
-      if (v->valid())  	{
-	cout << "Vertex XYZ is "
-             << v->vx()
-             << ", "
-             << v->vy()
-             << ", "
-             << v->vz()
-             << ", Chi2 is "
-             << v->chi2()
-             << ", Ntracks is "
-             << v->ntracks() << endl;  
-      }
-      delete t2; t2=0;
-      delete v;  v=0;
-    }
-    delete t1; t1=0;
-  }
-
-}
