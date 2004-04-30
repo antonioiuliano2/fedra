e481c3f491d2636fead262483138b206a3fd02dc
valeri <valeri@96872706-0045-0410-9534-84be8447c86c>
2004-04-30 07:58:17 +0000

    liner fit function by Yury
    git-svn-id: http://emulsion.na.infn.it/svn/fedra/trunk@242 96872706-0045-0410-9534-84be8447c86c

--- src/libEmath/EdbMath.cxx
+++ src/libEmath/EdbMath.cxx
@@ -49,3 +49,49 @@ double EdbMath::DistancePointLine3( float Point[3], float LineStart[3], float Li
  
     return Magnitude3( Point, Intersection );
 }
+
+//-------------------------------------------------------------------------------------
+bool EdbMath::LinFitDiag( int n, float *x, float *y, float *e, float p[2], float d[2][2], float *chi2)
+{
+  // linear fit by YP:  y = p[0] + p[1]*x , d[2][2] is error matrix for p[2]
+  // Input : n     - number of points
+  //         x,y,e - data arrays of the length n
+    
+    if (n < 2) return false;
+    int i = 0;
+    double w = 0., ed = 0.;
+    double sw = 0., swx = 0., swx2 = 0., swy = 0., swxy = 0.;
+    float dy = 0.;
+
+    for (i=0; i<n; i++)
+    {
+	ed = (double)e[i];
+	if (ed > 0.)
+	{
+	    w = 1./(ed*ed);
+	    sw   += w;
+	    swx  += w*x[i];
+	    swx2 += w*x[i]*x[i];
+	    swy  += w*y[i];
+	    swxy += w*x[i]*y[i];
+	}
+    }
+    double det = sw*swx2 - swx*swx;
+    if (det == 0.) return false;
+    d[0][0] = (float)( swx2/det);
+    d[0][1] = (float)(-swx/det);
+    d[1][0] = d[0][1];
+    d[1][1] = (float)(sw/det);
+    p[0] = (float)((swy*swx2 - swxy*swx)/det);
+    p[1] = (float)((swxy*sw  - swy*swx)/det);
+    *chi2 = 0.;
+    for (i=0; i<n; i++)
+    {
+	if ( e[i] > 0.)
+	{
+	    dy = y[i] - p[0] - p[1]*x[i];
+	    *chi2 += dy*dy/(e[i]*e[i]);
+	}
+    }
+    return true;
+}
--- src/libEmath/EdbMath.h
+++ src/libEmath/EdbMath.h
@@ -20,6 +20,8 @@ class EdbMath {
 
   static double Magnitude3( float Point1[3], float Point2[3] );
   static double DistancePointLine3( float Point[3], float LineStart[3], float LineEnd[3], bool inside );
+  static bool LinFitDiag( int n, float *x, float *y, float *e, 
+			  float p[2], float d[2][2], float *chi2);
 
   ClassDef(EdbMath,1)  // general matematical algorithms
 };
