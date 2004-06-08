5ed3ba9aa957b0b64f0eeff471778bba7b32b09d
valeri <valeri@96872706-0045-0410-9534-84be8447c86c>
2004-06-08 03:18:11 +0000

    separated Monte-Carlo library
    git-svn-id: http://emulsion.na.infn.it/svn/fedra/trunk@282 96872706-0045-0410-9534-84be8447c86c

---
+++ src/libEMC/EdbPVGen.h
@@ -0,0 +1,74 @@
+#ifndef ROOT_EdbPVGen
+#define ROOT_EdbPVGen
+ 
+//////////////////////////////////////////////////////////////////////////
+//                                                                      //
+// EdbPVGen - Patterns Volume Generator                                 //
+//                                                                      //
+//  collection of agorithms                                             //
+//                                                                      //
+//////////////////////////////////////////////////////////////////////////
+#include "TGenPhaseSpace.h"
+#include "EdbPVRec.h"
+
+//______________________________________________________________________________
+class EdbPVGen : public TObject {
+ 
+ private:
+
+  EdbPatternsVolume *ePVolume;
+
+  EdbScanCond *eScanCond;
+
+ public:
+  TObjArray   *eTracks;
+  TObjArray   *eVTX;          //
+
+ public:
+  EdbPVGen();
+  ~EdbPVGen();
+
+  void SetVolume( EdbPatternsVolume *pv) {ePVolume=pv;}
+  void SetScanCond( EdbScanCond *scan) {eScanCond=scan;}
+  EdbPatternsVolume *GetVolume() const { return ePVolume;}
+
+  void SmearSegments();
+  void SmearPatterns(float shift, float rot);
+
+  void GenerateBeam( int n, float x[4], float sx[4], float lim[4], float z0, int flag=0 );
+  void GenerateBackground( int n, float x[4], float sx[4], int flag=0 );
+
+  void GeneratePulsPoisson( float mean, float amp=1., float wmin=0, float wmax=0., int flag=0 );
+  void GeneratePulsGaus( float amp, float mean, float sigma, float wmin=0, float wmax=0., int flag=0 );
+
+  void AddTrack(EdbTrackP *track) {
+    if(!eTracks) eTracks = new TObjArray();
+    eTracks->Add(track);
+  }
+
+  void AddVertex(EdbVertex *vtx) {
+    if(!eVTX) eVTX = new TObjArray();
+    eVTX->Add((TObject*)vtx);
+  }
+
+
+  void TrackMC( float zlim[2],
+		float lim[4], float sigma[4], 
+		EdbTrackP &tr, int eloss_flag = 0, float PGap = 0. );
+
+  void GenerateUncorrelatedSegments(int nb, float lim[4], float sig[4],
+				    float TetaMax, int flag );
+
+  void GenerateBackgroundTracks(int nb, float lim[4], float plim[2],
+				float sig[4], float TetaMax,
+				float ProbGap, int eloss_flag );
+
+  void GeneratePhaseSpaceEvents( int nv, TGenPhaseSpace *pDecay, float zlim[2],
+				 float lim[4],    float sig[4],
+				 float ProbGap,   int eloss_flag,
+				 int prim_charge );
+
+  ClassDef(EdbPVGen,1)  // PatternsVolume Generator
+};
+
+#endif /* ROOT_EdbPVGen */
