f7640c9c77a597e777682f9fadcae2a8e28bc483
valeri <valeri@96872706-0045-0410-9534-84be8447c86c>
2004-06-30 05:44:16 +0000

    new option for rigid patterns
    git-svn-id: http://emulsion.na.infn.it/svn/fedra/trunk@286 96872706-0045-0410-9534-84be8447c86c

--- src/appl/recset/recset.cpp
+++ src/appl/recset/recset.cpp
@@ -15,8 +15,8 @@ int main(int argc, char* argv[])
       cout<< "\t\t  -ccd  - to remove ccd defects (update par/xxx.par file)\n";
       cout<< "\t\t  -l    - link up/down\n";
       cout<< "\t\t  -ang  - correct up/down angles offset and rotations\n";
-      cout<< "\t\t  -a    - plate to plate alignment\n";
-      cout<< "\t\t  -f    - fine alignment based on passed-throw tracks\n";
+      cout<< "\t\t  -a[n] - plate to plate alignment (if n=2: rigid patterns) \n";
+      cout<< "\t\t  -f[n] - fine alignment based on passed-throw tracks (if n=2: rigid patterns)\n";
       cout<< "\t\t  -t[n] - tracking (if n>1, holes isertion started - historical option - do not recommended!)\n";
       cout<< "\t\t  -t -p[p] - tracking&propagation (p is the momentum of the particle)\n";
       //      cout<< "\t\t  -rt  - raw tracking \n";
@@ -34,37 +34,51 @@ int main(int argc, char* argv[])
 
   for(int i=1; i<argc-1; i++ ) {
     char *key  = argv[i];
-    if(!strcmp(key,"-l"))    doLink=1;
-    if(!strcmp(key,"-a"))    doAlign=1;
+
+    if(!strcmp(key,"-l"))                doLink=1;
+
+    if(!strncmp(key,"-a",2)) {
+      if(strlen(key)>2)
+	sscanf(key+2,"%d",&doAlign);
+      if(doAlign==0)                     doAlign=1;
+    }
+
+    if(!strncmp(key,"-f",2)) {
+      if(strlen(key)>2)
+	sscanf(key+2,"%d",&doFine);
+      if(doFine==0)                     doFine=1;
+    }
+
     if(!strncmp(key,"-t",2)) {
       if(strlen(key)>2)
 	sscanf(key+2,"%d",&doTrack);
-      if(doTrack==0) doTrack=1;
+      if(doTrack==0)                     doTrack=1;
     }
+
     if(!strncmp(key,"-p",2)) {
       if(strlen(key)>2)
 	sscanf(key+2,"%f",&doPropagation);
     }
+
     if(!strcmp(key,"-ccd"))  doCCD=1;
     if(!strcmp(key,"-ang"))  doAngles=1;
-    if(!strcmp(key,"-f"))    doFine=1;
     if(!strcmp(key,"-rt"))   doRaw=1;
     if(!strcmp(key,"-nu"))   noUpdate=1;
   }
 
-  printf("%d %d %d %d %d %d %d %d %s\n",
-	 doCCD, doLink, doAlign, doTrack, doFine, doAngles, doRaw, noUpdate, name);
+  printf("%d %d %d %d %d %d %d %f %d %s\n",
+	 doCCD, doLink, doAlign, doTrack, doFine, doAngles, doRaw, doPropagation, noUpdate, name);
 
   EdbDataProc proc(name);
 
   proc.SetNoUpdate(noUpdate);
   if(doCCD)              { proc.CheckCCD();               doCCD=0; }
   if(doLink)             { proc.Link();                   doLink=0; }
-  if(doTrack&&doAlign)   { proc.AlignLinkTracks(doTrack); doTrack=0; doAlign=0; }
-  if(doAlign)            { proc.Align();                  doAlign=0; }
+  if(doTrack&&doAlign)   { proc.AlignLinkTracks(doTrack,doAlign); doTrack=0; doAlign=0; }
+  if(doAlign)            { proc.Align(doAlign);           doAlign=0; }
   if(doAngles)           { proc.CorrectAngles();          doAngles=0; }
   if(doTrack)            { proc.LinkTracks(doTrack, doPropagation); doTrack=0; doPropagation=-1; }
-  if(doFine)             { proc.FineAlignment();          doFine=0; }
+  if(doFine)             { proc.FineAlignment(doFine);    doFine=0; }
   if(doRaw)              { proc.LinkRawTracks(doRaw);     doRaw=0; }
 
   return 0;
