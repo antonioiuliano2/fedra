e150c4bc9821f8456fd0480d9216f0e71ca74aed
valeri <valeri@96872706-0045-0410-9534-84be8447c86c>
2005-07-01 12:42:01 +0000

    bug fixed in ReadTracksTree
    git-svn-id: http://emulsion.na.infn.it/svn/fedra/trunk@480 96872706-0045-0410-9534-84be8447c86c

--- src/libEIO/EdbDataSet.cxx
+++ src/libEIO/EdbDataSet.cxx
@@ -2513,7 +2513,7 @@ int EdbDataProc::ReadTracksTree( EdbPVRec &ali,
     entr = lst->GetEntry(j);
     tracks->GetEntry(entr);
 
-    EdbTrackP *tr1 = new EdbTrackP();
+    EdbTrackP *tr1 = new EdbTrackP( trk, 0.124 );  // TODO: the default mass for the time being
 
     tr1->SetID(trk->ID());
     tr1->SetP(trk->P());
