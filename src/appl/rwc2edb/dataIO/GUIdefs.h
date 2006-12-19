96622aabc4a41f11513f27208d06c8d6d9316c16
valeri <valeri@96872706-0045-0410-9534-84be8447c86c>
2006-12-19 10:38:04 +0000

    removed dataIO from rwc2edb
    git-svn-id: http://emulsion.na.infn.it/svn/fedra/trunk@612 96872706-0045-0410-9534-84be8447c86c

--- src/appl/rwc2edb/dataIO/GUIdefs.h
+++
@@ -1,9 +0,0 @@
-#ifndef _SYSAL_GUI_DEFINITIONS_
-#define _SYSAL_GUI_DEFINITIONS_
-
-typedef long HSySalHANDLE;
-typedef long HSySalHICON;
-typedef long HSySalHBITMAP;
-typedef long HSySalHWND;
-
-#endif
