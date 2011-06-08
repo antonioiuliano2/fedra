87aad703ebb62ca29174a8981ffb44313540d7f7
valeri <valeri@96872706-0045-0410-9534-84be8447c86c>
2011-06-08 09:48:20 +0000

    comment out declarations of 2 functions added by Frank without definition. this makes possible applications building again
    git-svn-id: http://emulsion.na.infn.it/svn/fedra/trunk@1165 96872706-0045-0410-9534-84be8447c86c

--- src/libEdr/EdbPattern.h
+++ src/libEdr/EdbPattern.h
@@ -355,8 +355,8 @@ class EdbPatternsVolume : public TObject {
   void         AddPatternAt( EdbPattern *pat, int id );
   EdbPattern  *GetPattern( int id ) const;
   EdbPattern  *GetPatternZLowestHighest(Bool_t lowestZ=kTRUE) const;
-  EdbPattern  *GetPatternZLowest() const;
-  EdbPattern  *GetPatternZHighest() const;
+  //  EdbPattern  *GetPatternZLowest() const;
+  //  EdbPattern  *GetPatternZHighest() const;
   
 
   EdbPattern  *NextPattern(float z, int dir) const;
