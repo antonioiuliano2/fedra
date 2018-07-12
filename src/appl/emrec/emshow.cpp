bfaead4a12adc01a331ac1418a2976b9cff73f89
meisel <meisel@96872706-0045-0410-9534-84be8447c86c>
2018-07-12 09:50:59 +0000

    Prepare "emshow" to be standard quick reco program, so no cumbersome root scripts have to be written from scratch.
    git-svn-id: http://emulsion.na.infn.it/svn/fedra/trunk@1474 96872706-0045-0410-9534-84be8447c86c

--- src/appl/emrec/emshow.cpp
+++ src/appl/emrec/emshow.cpp
@@ -11,11 +11,11 @@ using namespace std;
 void print_help_message()
 {
     cout<< "\nUsage: \n\t  emshow -set=ID [ -o=DATA_DIRECTORY -v=DEBUG] \n\n";
-    cout<< "\t\t  ID    - id of the dataset formed as BRICK.PLATE.MAJOR.MINOR \n";
-    cout<< "\t\t  DEBUG - verbosity level: 0-print nothing, 1-errors only, 2-normal, 3-print all messages\n";
+    cout<< "\t\t  -set=ID   - id of the dataset formed as BRICK.PLATE.MAJOR.MINOR \n";
+    cout<< "\t\t  DEBUG     - verbosity level: 0-print nothing, 1-errors only, 2-normal, 3-print all messages\n";
     cout<< "\t\t  -vt    - start showering from linked tracks attached to vertices with IP<250 (not yet included)\n";
     cout<< "\t\t  -lt    - start showering from linked tracks (all; standard)\n";
-    cout<< "\t\t  -bt    - start showering from basetracks (all; usefull if no linked tracks done yet)\n";
+    cout<< "\t\t  -bt    - start showering from basetracks (all; useful if no linked tracks done yet)\n";
     cout<< "\nExample: \n";
     cout<< "\t  emshow -id=4554.10.1.0 -o/scratch/BRICKS -lt\n";
     cout<< "\n  The data location directory if not explicitly defined will be taken from .rootrc as: \n";
@@ -26,19 +26,20 @@ void print_help_message()
 
 void set_default(TEnv &cenv)
 {
-  // default parameters for shower reconstruction
-  cenv.SetValue("emshow.cpcut" ,"s.eW>13&&eCHI2P<2.5&&s1.eFlag>=0&&s2.eFlag>=0&&eN1==1&&eN2==1");
-/*  cenv.SetValue("fedra.track.minPlate"  ,-999 );
-  cenv.SetValue("fedra.track.maxPlate"  , 999 );
-  cenv.SetValue("fedra.track.refPlate"  , 999 );
-  cenv.SetValue("fedra.track.nsegmin"   , 2 );
-  cenv.SetValue("fedra.track.ngapmax"   , 4 );
-  cenv.SetValue("fedra.track.probmin"   , 0.01 );
-  cenv.SetValue("fedra.track.momentum"  , 2 );
-  cenv.SetValue("fedra.track.mass"      , 0.14 );*/
-  cenv.SetValue("emshow.outdir"          , "..");
-  cenv.SetValue("emshow.env"             , "shower.rootrc");
-  cenv.SetValue("emshow.EdbDebugLevel"   , 1);
+    // default parameters for shower reconstruction
+    // determined by experimental and simulation studies
+    cenv.SetValue("emshow.cpcut","s.eW>13&&eCHI2P<2.5&&s1.eFlag>=0&&s2.eFlag>=0&&eN1==1&&eN2==1");
+    /*  cenv.SetValue("fedra.track.minPlate"  ,-999 );
+      cenv.SetValue("fedra.track.maxPlate"  , 999 );
+      cenv.SetValue("fedra.track.refPlate"  , 999 );
+      cenv.SetValue("fedra.track.nsegmin"   , 2 );
+      cenv.SetValue("fedra.track.ngapmax"   , 4 );
+      cenv.SetValue("fedra.track.probmin"   , 0.01 );
+      cenv.SetValue("fedra.track.momentum"  , 2 );
+      cenv.SetValue("fedra.track.mass"      , 0.14 );*/
+    cenv.SetValue("emshow.outdir", "..");
+    cenv.SetValue("emshow.env", "shower.rootrc");
+    cenv.SetValue("emshow.EdbDebugLevel", 1);
 }
 
 int main(int argc, char* argv[])
@@ -50,17 +51,17 @@ int main(int argc, char* argv[])
 
     TEnv cenv("showerenv");
     set_default(cenv);
-    gEDBDEBUGLEVEL        = cenv.GetValue("emshow.EdbDebugLevel" , 1);
-    const char *env       = cenv.GetValue("emshow.env"            , "shower.rootrc");
-    const char *outdir    = cenv.GetValue("emshow.outdir"         , "./");
+    gEDBDEBUGLEVEL        = cenv.GetValue("emshow.EdbDebugLevel", 1);
+    const char *env       = cenv.GetValue("emshow.env", "shower.rootrc");
+    const char *outdir    = cenv.GetValue("emshow.outdir", "./");
 
     bool      do_set      = false;
     bool      do_pred     = false;
     bool      do_VSB      = false;
-		bool      do_showerfrom_vt      = false;
-		bool      do_showerfrom_lt      = false;
-		bool      do_showerfrom_bt      = false;
-		
+    bool      do_showerfrom_vt      = false;
+    bool      do_showerfrom_lt      = false;
+    bool      do_showerfrom_bt      = false;
+
     Int_t     pred_plate  = 0, to_plate=0;
     Int_t     brick=0, plate=0, major=0, minor=0;
 
@@ -119,9 +120,9 @@ int main(int argc, char* argv[])
         return 0;
     }
 
-    cenv.SetValue("emshow.env"                  , env);
-    cenv.ReadFile( cenv.GetValue("emshow.env"   , "shower.rootrc") ,kEnvLocal);
-    cenv.SetValue("emshow.outdir"               , outdir);
+    cenv.SetValue("emshow.env", env);
+    cenv.ReadFile( cenv.GetValue("emshow.env", "shower.rootrc"),kEnvLocal);
+    cenv.SetValue("emshow.outdir", outdir);
     cenv.WriteFile("shower.save.rootrc");
 
     if (do_set) {
@@ -137,12 +138,12 @@ int main(int argc, char* argv[])
         //ss->MakePIDList();
         //sproc.AssembleScanSet(*ss);
 
-        TCut c = gEnv->GetValue("emshow.cpcut" ,"s.eW>13&&eCHI2P<2.5&&s1.eFlag>=0&&s2.eFlag>=0&&eN1==1&&eN2==1");
+        TCut c = gEnv->GetValue("emshow.cpcut","s.eW>13&&eCHI2P<2.5&&s1.eFlag>=0&&s2.eFlag>=0&&eN1==1&&eN2==1");
 
         //EdbScanCond cond;
         //cond.Print();
         //sproc.TrackSetBT(*ss,cond,c);
-	sproc.TrackSetBT(*ss,cenv);
+        sproc.TrackSetBT(*ss,cenv);
 
     }
     if (do_VSB) {
@@ -156,7 +157,6 @@ int main(int argc, char* argv[])
         EdbScanSet *ss = sproc.ReadScanSet(id);
         ss->Brick().SetID(brick);
         sproc.AssembleScanSet(*ss);
-
 
         EdbID predid(id);
         predid.ePlate = pred_plate;
